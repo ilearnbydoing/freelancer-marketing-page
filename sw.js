cf4447c84896e07770cb6329810c9a9cf7a26f58
Durgesh Gupta <dgweb123@gmail.com>
2019-08-22 11:17:48 +0530

    PWA Update

--- sw.js
+++ sw.js
@@ -1,9 +1,2 @@
-var urlsToCache = [];
-
-// Cache posts
-// Limits the number of posts that gets cached to 3
-// Reads a piece of front-matter in each post that directs the second loop to the folder where the assets are held
-
-    urlsToCache.push("/");
-    
-        
+importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
+AMP_SW.init();

22ebcb65ed8c244d99bd6b05bf71c8c1e5a58caa
Durgesh Gupta <dgweb123@gmail.com>
2019-08-22 10:56:27 +0530

    update

--- sw.js
+++ sw.js
@@ -3,3 +3,7 @@ var urlsToCache = [];
 // Cache posts
 // Limits the number of posts that gets cached to 3
 // Reads a piece of front-matter in each post that directs the second loop to the folder where the assets are held
+
+    urlsToCache.push("/");
+    
+        
